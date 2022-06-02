7afcd414ecd8eeb7c5e0809bb4c571983f6fc31a
marchenkomv <marchenko_m.v@mail.ru>
2022-06-02 18:35:47 +1000

    initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,51 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepository {
+
+    private ConcurrentMap<Long, Post> repository;
+    private AtomicLong counter;
+    private final long COUNTER_INITIAL_VALUE = 1;
+
+    public PostRepository() {
+        repository = new ConcurrentHashMap<>();
+        counter = new AtomicLong(COUNTER_INITIAL_VALUE);
+    }
+
+    public List<Post> all() {
+        return new ArrayList<>(repository.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(repository.get(id));
+    }
+
+    public Optional<Post> save(Post post) {
+        if (post.getId() == 0L) {
+            var key = counter.getAndIncrement();
+            post.setId(key);
+            repository.put(key, post);
+        } else {
+            if (repository.containsKey(post.getId())) {
+                repository.get(post.getId()).setContent(post.getContent());
+            } else {
+                post = null;
+            }
+        }
+        return Optional.ofNullable(post);
+    }
+
+    public Optional<Post> removeById(long id) {
+        return Optional.ofNullable(repository.remove(id));
+    }
+}
